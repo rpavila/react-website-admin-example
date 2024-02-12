9515ec9f2553211885048e09e3ef95bda3201035
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-12 00:23:37 -0600

    Implement routing and replace placeholder content in App.js
    Updated App.js to replace the placeholder content with functioning routing. Imported Router, Route, and Routes from "react-router-dom" to enable navigation within the app. Additionally, removed importation of unused 'logo.svg' and added import '@mantine/core/styles.css' for styling.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import '@mantine/core/styles.css';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import Home from './Home';
+import Login from './components/login/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path={'/'}>
+                    <Route index element={<Home/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
