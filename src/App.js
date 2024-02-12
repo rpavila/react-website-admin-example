57dc46e617413621737db3356e327f562cd17f13
Ricardo Pablo Avila Alfaro <ravila@grupodeca.com.mx>
2024-02-12 09:56:37 -0600

    Refactor routing in App.js
    This commit updates the routing implementation in App.js, replacing BrowserRouter with createBrowserRouter and RouterProvider. Additionally, it defines a new router structure with nested routes and asynchronous route loading capabilities via the lazy function for improved application performance.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import '@mantine/core/styles.css';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/home/Home";
 
-import Home from './Home';
-import Login from './components/login/Login';
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "login",
+                // Single route in lazy file
+                lazy: () => import("./components/login/Login"),
+            },
+            {
+                path: "dashboard",
+                async lazy() {
+                    // Multiple routes in lazy file
+                    let { DashboardLayout: Dashboard } = await import("./components/dashboard/Dashboard");
+                    return { Component: Dashboard };
+                },
+                children: [
+                    {
+                        index: true,
+                        async lazy() {
+                            let { DashboardIndex } = await import("./components/dashboard/Dashboard");
+                            return { Component: DashboardIndex };
+                        },
+                    },
+                    // {
+                    //     path: "messages",
+                    //     async lazy() {
+                    //         let { dashboardMessagesLoader, DashboardMessages } = await import(
+                    //             "./pages/dashboard"
+                    //             );
+                    //         return {
+                    //             loader: dashboardMessagesLoader,
+                    //             Component: DashboardMessages,
+                    //         };
+                    //     },
+                    // },
+                ],
+            },
+            // {
+            //     path: "*",
+            //     element: <NoMatch />,
+            // },
+        ],
+    },
+]);
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path={'/'}>
-                    <Route index element={<Home/>}/>
-                    <Route path={'/login'} element={<Login/>}/>
-                </Route>
-            </Routes>
-        </Router>
+        <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
     );
 }
 
