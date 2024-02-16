1d88fa846f74c036a8a52972aad251aa61b627cd
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-16 00:44:22 -0600

    Update user redirection path and rename login files
    Updated the user redirection path from '/login' to '/auth/login' to tackle routing problems in middleware.ts. Additionally, renamed and moved login-related files to 'auth/login' directory for better organization of the authentication related files.

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,7 @@ export async function middleware(req: NextRequest) {
 
     // if user is not signed in and the current path is not / redirect the user to /
     if (!user) {
-        return NextResponse.redirect(new URL('/login', req.url))
+        return NextResponse.redirect(new URL('/auth/login', req.url))
     }
 
     return res
