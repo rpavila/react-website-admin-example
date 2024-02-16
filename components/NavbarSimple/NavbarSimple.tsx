422ff91ecd83c3de814387fd75597680cd50dede
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-16 00:44:06 -0600

    Update NavbarSimple component for signout
    Added useRef and a form for signout functionality in the NavbarSimple component. The "Change account" option has been commented out. A "Logout" link now triggers the new function handleClick which, on click event, submits the referenced form leading to the signout route.

--- components/NavbarSimple/NavbarSimple.tsx
+++ components/NavbarSimple/NavbarSimple.tsx
@@ -1,15 +1,16 @@
 'use client'
-import {useState} from 'react';
-import {IconHome, IconLogout, IconSwitchHorizontal, IconUserHexagon,} from '@tabler/icons-react';
+import {useRef, useState} from 'react';
+import {IconHome, IconLogout, IconUserHexagon,} from '@tabler/icons-react';
 import classes from './NavbarSimple.module.css';
 import Link from "next/link";
 
 const data = [
-    { link: '/dashboard', label: 'Home', icon: IconHome },
-    { link: '/customers', label: 'Customers', icon: IconUserHexagon },
+    {link: '/dashboard', label: 'Home', icon: IconHome},
+    {link: '/customers', label: 'Customers', icon: IconUserHexagon},
 ];
 
 export default function NavbarSimple() {
+    const formRef = useRef();
     const [active, setActive] = useState('Billing');
 
     const links = data.map((item) => (
@@ -19,10 +20,17 @@ export default function NavbarSimple() {
             href={item.link}
             key={item.label}
         >
-            <item.icon className={classes.linkIcon} stroke={1.5} />
+            <item.icon className={classes.linkIcon} stroke={1.5}/>
             <span>{item.label}</span>
         </Link>
     ));
+
+    const handleClick = (event: any) => {
+        event.preventDefault()
+        if(formRef.current) {
+            formRef.current.submit()
+        }
+    }
 
     return (
         <>
@@ -31,13 +39,15 @@ export default function NavbarSimple() {
             </div>
 
             <div className={classes.footer}>
-                <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-                    <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-                    <span>Change account</span>
-                </a>
+                {/*<a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>*/}
+                {/*    <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5}/>*/}
+                {/*    <span>Change account</span>*/}
+                {/*</a>*/}
 
-                <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-                    <IconLogout className={classes.linkIcon} stroke={1.5} />
+                <form ref={formRef} action="/auth/signout" method="post"></form>
+
+                <a href="#" className={classes.link} onClick={handleClick}>
+                    <IconLogout className={classes.linkIcon} stroke={1.5}/>
                     <span>Logout</span>
                 </a>
             </div>
