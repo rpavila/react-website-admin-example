17d34037e8f3f647629afe33790c65215c6ef3b8
Ricardo Pablo Avila Alfaro <ravila@grupodeca.com.mx>
2024-02-16 09:20:40 -0600

    Add UserButton component and associated styles
    Introduced a new UserButton component in UserButton.tsx file with related styles in UserButton.module.css file. The component includes a user avatar, the user's display name and email, a form to handle signout and a collapsible logout link. The associated CSS file contains style rules specific to this component's unique elements.

---
+++ components/UserButton/UserButton.tsx
@@ -0,0 +1,49 @@
+import {UnstyledButton, Group, Avatar, Text, rem, Collapse} from '@mantine/core';
+import {IconChevronRight, IconLogout} from '@tabler/icons-react';
+import classes from './UserButton.module.css';
+import {useRef, useState} from "react";
+import {User} from "@supabase/gotrue-js";
+
+export function UserButton({ user }: { user: User }) {
+    const formRef = useRef();
+    const [opened, setOpened] = useState( false);
+
+    const handleClick = (event: any) => {
+        event.preventDefault()
+        if(formRef.current) {
+            formRef.current.submit()
+        }
+    }
+
+    return (
+        <>
+            <UnstyledButton onClick={() => setOpened((o) => !o)} className={classes.user}>
+                <Group>
+                    <Avatar
+                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+                        radius="xl"
+                    />
+
+                    <div style={{flex: 1}}>
+                        {user.user_metadata?.display_name && <Text size="sm" fw={500}>
+                            {user.user_metadata?.display_name}
+                        </Text>}
+
+                        <Text c="dimmed" size="xs">
+                            {user.email}
+                        </Text>
+                    </div>
+
+                    <IconChevronRight style={{width: rem(14), height: rem(14)}} stroke={1.5}/>
+                </Group>
+            </UnstyledButton>
+            <form ref={formRef} action="/auth/signout" method="post"></form>
+            <Collapse in={opened}>
+                <a href="#" className={classes.link} onClick={handleClick}>
+                    <IconLogout className={classes.linkIcon} stroke={1.5}/>
+                    <span>Logout</span>
+                </a>
+            </Collapse>
+        </>
+    );
+}
