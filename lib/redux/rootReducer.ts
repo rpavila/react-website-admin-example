9ed26d00a0aa5b38ade5360360df4aa338495c2e
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-22 21:49:45 -0600

    Remove console.log calls from rootReducer
    The logs serving debug purposes were removed from rootReducer.ts for cleaner, production-ready code. The minor update does not affect functionality or performance but contributes towards maintaining clean code practices.

--- lib/redux/rootReducer.ts
+++ lib/redux/rootReducer.ts
@@ -2,10 +2,7 @@
 import { customerApi } from "@/lib/redux/api/customerApi";
 import authSlice from "./slices/auth/authSlice";
 
-console.log(authSlice)
-// console.log(customerApi)
 export const reducer = {
-    // counter: counterSlice.reducer,
     [customerApi.reducerPath]: customerApi.reducer,
     auth: authSlice,
 };
