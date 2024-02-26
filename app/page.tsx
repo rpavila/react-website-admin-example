b78a6e2e7e6c8bce2e4867552c8fd45b3fc9de2d
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-25 22:09:43 -0600

    Refactor Page component to integrate authentication
    The Page.tsx file was refactored to include authentication logic with the use of the Supabase client. An auth check is implemented so that if a user is not authenticated, they are redirected to the login page. This enhances the security by enforcing authentication for users accessing the HomePage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-import HomePage from "../components/HomePage/HomePage";
+import HomePage from "@/components/HomePage/HomePage";
+import {createClient} from "@/lib/utils/supabase/server";
+import {redirect} from "next/navigation";
 
 
-export default HomePage;
+export default async function Page(){
+
+    const supabase = createClient()
+
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        redirect('/auth/login')
+    }
+
+    return (<HomePage />)
+}
