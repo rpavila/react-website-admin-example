deb7606023cab5c7fb7444a0034d0b1ddc093ebe
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-25 22:10:40 -0600

    Refactor LoginForm to utilize useSearchParams and useEffect
    This commit simplifies the LoginForm component by replacing useRouter and createClientComponentClient methods with useSearchParams and useEffect. It also removes a few unused import statements, updating logic for displaying any error messages using the useEffect function. The code is restructured to be cleaner and more efficient with enhanced error notifications.

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,22 +1,18 @@
 'use client'
-import {useRouter} from "next/navigation";
-import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {useSearchParams} from "next/navigation";
 import {useDisclosure} from "@mantine/hooks";
 import {z} from "zod";
 import {useForm} from "@mantine/form";
 import {zodResolver} from "mantine-form-zod-resolver";
-import {cleanNotifications, notifications} from "@mantine/notifications";
-import {NotificationData} from "@mantine/notifications/lib/notifications.store";
+import {notifications} from "@mantine/notifications";
 import {Anchor, Button, Checkbox, Container, Group, Paper, PasswordInput, Text, TextInput, Title} from "@mantine/core";
 import classes from "@/components/LoginForm/LoginForm.module.css";
-import {Database} from "@/lib/database.types";
-import { setSession, useDispatch } from "@/lib/redux";
+import {login} from "@/app/auth/login/actions";
+import {useEffect} from "react";
 
 
 export default function LoginForm() {
-    const router = useRouter()
-    const dispatch = useDispatch()
-    const supabase = createClientComponentClient<Database>()
+    const searchParams = useSearchParams()
     const [loading, {toggle}] = useDisclosure(false)
     const schema = z.object({
         email: z.string({required_error: 'This field is required'}).email({message: 'Invalid email'}),
@@ -30,72 +26,58 @@ export default function LoginForm() {
         validate: zodResolver(schema),
     });
 
-    const onSubmit = (values: any) => {
-        cleanNotifications()
-        toggle()
-        supabase.auth.signInWithPassword(values).then((r) => {
-            let messageData: NotificationData = {
-                autoClose: true,
-                message: '',
-                color: 'green',
-                style: {
-                    position: 'fixed',
-                    top: '1rem',
-                    right: '1rem'
-                }
-            }
-            const {error, data: { session } } = r
+    useEffect(() => {
+        if (searchParams.size > 0) {
+            const error = searchParams.get('error')
+            console.log(error)
             if (error) {
-                const {name: title, message} = error
-                messageData = {
-                    ...messageData,
-                    title, message, color: 'red'
-                }
-            } else {
-                messageData = {
-                    ...messageData,
-                    title: 'LoginForm successfully', message: 'User logged'
-                }
+                notifications.show({
+                    autoClose: true,
+                    style: {
+                        position: 'fixed',
+                        top: '1rem',
+                        right: '1rem'
+                    },
+                    title: 'error',
+                    message: error,
+                    color: 'red'
+                })
             }
-            notifications.show(messageData)
-            if(!error){
-                dispatch(setSession(session))
-                cleanNotifications()
-                router.push('/dashboard')
-            }
-        })
-    }
+        }
+    }, [searchParams]);
 
     return (
         <>
             <Container size={420} my={40}>
+
                 <Title ta="center" className={classes.title}>
                     Welcome back!
                 </Title>
-                <Text c="dimmed" size="sm" ta="center" mt={5}>
-                    Do not have an account yet?{' '}
-                    <Anchor size="sm" component="button">
-                        Create account
-                    </Anchor>
-                </Text>
+                {/*<Text c="dimmed" size="sm" ta="center" mt={5}>*/}
+                {/*    Do not have an account yet?{' '}*/}
+                {/*    <Anchor size="sm" component="button">*/}
+                {/*        Create account*/}
+                {/*    </Anchor>*/}
+                {/*</Text>*/}
 
                 <Paper withBorder shadow="lg" p={30} mt={30} radius="md">
-                    <form onSubmit={form.onSubmit(onSubmit)}>
-                        <TextInput label="Email" placeholder="you@mantine.dev" {...form.getInputProps('email')}
+                    <form>
+                        <TextInput label="Email" name={'email'}
+                                   placeholder="you@mantine.dev" {...form.getInputProps('email')}
                                    onChange={(e) => {
                                        form.setValues({email: e.currentTarget.value})
                                    }}/>
-                        <PasswordInput label="Password" placeholder="Your password"
+                        <PasswordInput label="Password" name={'password'} placeholder="Your password"
                                        mt="md" {...form.getInputProps('password')} onChange={(e) => {
                             form.setValues({password: e.currentTarget.value})
                         }}/>
                         <Group justify="space-between" mt="lg">
                             <Checkbox label="Remember me"/>
-                            <Anchor component="button" size="sm">
-                                Forgot password?
-                            </Anchor>
+                            {/*<Anchor component="button" size="sm">*/}
+                            {/*    Forgot password?*/}
+                            {/*</Anchor>*/}
                         </Group>
-                        <Button type={'submit'} fullWidth mt="xl" loading={loading}>
+                        <Button type={'submit'} fullWidth mt="xl" loading={loading} formAction={login}>
                             Sign in
                         </Button>
                     </form>
