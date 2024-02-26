7e34a674f8522cd307578ba0fec3b5d3f9f73e50
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-25 22:09:10 -0600

    Refactor middleware to enhance authentication
    The middleware code is refactored to make use of a dedicated method for session updates. This simplifies the initial middleware function, removing conditional redirects and delegating the session logic to a separate 'updateSession' function. Also, new management for non-matching request paths is included, filtering out unwanted static file requests.

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,19 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { updateSession } from '@/lib/utils/supabase/middleware'
+import {NextRequest} from "next/server";
 
-import type { NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-    const res = NextResponse.next()
-    const supabase = createMiddlewareClient({ req, res })
-
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
-
-    // if user is signed in and the current path is / redirect the user to /account
-    if (user && req.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-    }
-
-    // if user is not signed in and the current path is not / redirect the user to /
-    if (!user) {
-        return NextResponse.redirect(new URL('/auth/login', req.url))
-    }
-
-    return res
+export async function middleware(request: NextRequest) {
+    return await updateSession(request)
 }
 
 export const config = {
-    matcher: ['/', '/dashboard'],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+    ],
 }
