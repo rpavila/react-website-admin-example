0e45879c6e1002bcca58651fcd2cb5fdb2e9a65b
Ricardo P. Avila <rpaa01@gmail.com>
2024-02-27 22:26:54 -0600

    Refactor customerApi for cleaner Supabase interaction
    In this revision, unnecessary parts of code were commented out and new methods were introduced to streamline interactions with Supabase. Critical upgrade was the use of direct Supabase client instead of a fetchBaseQuery via toolkit. Besides cleaner code, it opens up all the Supabase capabilities from now on.

--- lib/redux/api/customerApi.ts
+++ lib/redux/api/customerApi.ts
@@ -1,32 +1,43 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {ReduxState} from "@/lib/redux";
-import {Session} from "@supabase/gotrue-js";
+import {BaseQueryApi, createApi} from "@reduxjs/toolkit/query/react";
 import {Customer} from "@/lib/redux/model";
+import {createClient} from '@/lib/utils/supabase/client';
+import {QueryReturnValue} from "@reduxjs/toolkit/dist/query/baseQueryTypes";
+import {MaybePromise} from "@reduxjs/toolkit/dist/query/tsHelpers";
+
+
+const supabase = createClient()
 
 export const customerApi = createApi({
     reducerPath: 'customers',
     tagTypes: ['Customer'],
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1`,
-        prepareHeaders: (headers, {getState}) => {
-            // By default, if we have a token in the store, let's use that for authenticated requests
-            const {session}: { session: Session | null } = (getState() as ReduxState).auth
-            const {token_type}: {
-                access_token: string | null,
-                token_type: string | null
-            } = session || {access_token: null, token_type: null}
-            const access_token = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-            if (access_token) {
-                headers.set('apikey', access_token)
-                headers.set('authentication', `${token_type} ${access_token}`)
-            }
-            return headers
-        },
-    }),
+    // baseQuery: fetchBaseQuery({
+    //     baseUrl: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1`,
+    //     // prepareHeaders: (headers, {getState}) => {
+    //     //     // By default, if we have a token in the store, let's use that for authenticated requests
+    //     //     const {session}: { session: Session | null } = (getState() as ReduxState).auth
+    //     //     const {token_type}: {
+    //     //         access_token: string | null,
+    //     //         token_type: string | null
+    //     //     } = session || {access_token: null, token_type: null}
+    //     //     const access_token = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    //     //     if (access_token) {
+    //     //         headers.set('apikey', access_token)
+    //     //         headers.set('authentication', `${token_type} ${access_token}`)
+    //     //     }
+    //     //     return headers
+    //     // },
+    // }),
     endpoints: (builder) => ({
         getCustomers: builder.query({
-            query: () => '/customer',
-            providesTags: (result, error, id) => [{type: 'Customer', id}]
+            providesTags: (result, error, id) => [{type: 'Customer', id}],
+            queryFn: async () => {
+                // Supabase conveniently already has `data` and `error` fields
+                const {data, error} = await supabase.from('customer').select();
+                if (error) {
+                    return {error};
+                }
+                return {data};
+            }
         }),
         saveCustomer: builder.mutation<Customer, Partial<Customer> & Pick<Customer, 'id'>>({
             query: ({...params}) => ({
@@ -35,7 +46,10 @@ export const customerApi = createApi({
                 body: params
             })
         })
-    })
+    }),
+    baseQuery: function (args: any, api: BaseQueryApi, extraOptions: {}): MaybePromise<QueryReturnValue<unknown, unknown, {}>> {
+        throw new Error("Function not implemented.");
+    }
 })
 
 export const {useGetCustomersQuery, useSaveCustomerMutation} = customerApi
